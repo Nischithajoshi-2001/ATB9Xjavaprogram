cf0167cf3f4c2ed2a7f983642b30e11619f32e25
nischitha.j <nischithajoshi87@gmail.com>
2024-10-07 22:03:34 +0530

    All the fiels added

---
+++ src/OCT/ex_03102024/Lab003.java
@@ -0,0 +1,11 @@
+package OCT.ex_03102024;
+
+public class Lab003
+{
+    public static void main(String[]args)
+    {
+        System.out.println("2+3=5");
+        System.out.println("2-5+=-3");
+        System.out.println("2*5=15");
+    }
+}
