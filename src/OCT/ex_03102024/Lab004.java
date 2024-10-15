b1c5c7350560c14ccf081e925e4a1c8edef0e10b
nischitha.j <nischithajoshi87@gmail.com>
2024-10-15 15:27:04 +0530

    All the files added

--- src/OCT/ex_03102024/Lab004.java
+++ src/OCT/ex_03102024/Lab004.java
@@ -1,4 +1,12 @@
 package OCT.ex_03102024;
 
-public class Lab004 {
+public class Lab004
+{
+    public static void main(String[]args)
+    {
+        int a=10;
+
+        System.out.println(a);
+    }
+
 }
