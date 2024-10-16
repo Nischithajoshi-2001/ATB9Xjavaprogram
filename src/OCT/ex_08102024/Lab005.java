ea27f2996fef24473df7321c794010064c6853d0
nischitha.j <nischithajoshi87@gmail.com>
2024-10-16 14:10:32 +0530

    lab

---
+++ src/OCT/ex_08102024/Lab005.java
@@ -0,0 +1,36 @@
+package OCT.ex_08102024;
+
+public class Lab005
+{
+    public static void main (String[]args)
+    {
+        // primitive Data Type
+
+               //1. boolean
+        boolean  b = true;// it only accpect true or flase//
+              //2. Numeric types
+                     //a. integeral
+
+        byte t = 127 ;// small value when it comes to numeric
+        short s= 32073; // larger than the byte
+        int age = 23; // Most commonly used
+        long l= 65765378467578l; // lager than all
+        char grade = 'A'; // it is small when it comes to alhabets and it should always have single coates
+
+                       //b. Floating
+        float a= 3.14f; // after the number f is mandatory  small in decimal
+        double d = 118.987654321; // lagre in decimal
+
+
+
+        System.out.println(t);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(age);
+        System.out.println(l);
+        System.out.println(grade);
+        System.out.println(a);
+        System.out.println(d);
+
+    }
+}
---
+++ src/OCT/ex_08102024/Lab006.java
@@ -0,0 +1,19 @@
+package OCT.ex_08102024;
+
+public class Lab006
+{
+    public static void main(String[]args)
+    {
+        //Data_type Variable _name =Variable  value
+        // int age =24;
+        //int (Data type)
+        //age ( variable name/identifier )
+        // = ( assignment  operator)
+        //  24 (Variable value)
+
+       // age = age+1;
+        int age = 30;
+
+        System.out.println(age);
+    }
+}
